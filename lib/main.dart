d6494082c8ae0224d73a4196e8e84fc8407f695a
Rafael David <rafaelcapitaodavid@gmail.com>
2024-09-15 22:05:43 +0100

    - remove flutter_dotenv - remove .env - add .env.local.json - add .vscode to run --dart-define-from-file each time of debug/build to get keys

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
-import 'package:flutter_dotenv/flutter_dotenv.dart';
+/* import 'package:flutter_dotenv/flutter_dotenv.dart'; */
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await dotenv.load(fileName: ".env");
-  await Supabase.initialize(
-    url: dotenv.env['SUPABASE_URL']!,
-    anonKey: dotenv.env['SUPABASE_ANON_KEY']!,
-  );
+
+  /* await dotenv.load(fileName: ".env"); */
+  final SUPABASE_URL = const String.fromEnvironment('SUPABASE_URL');
+  final SUPABASE_ANON_KEY = const String.fromEnvironment('SUPABASE_ANON_KEY');
+
+  await Supabase.initialize(url: SUPABASE_URL, anonKey: SUPABASE_ANON_KEY);
   runApp(const MyApp());
 }
 
